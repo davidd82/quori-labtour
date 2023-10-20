3b7ed074d3061defc83f0332f3a08568afcab7d9
David Delgado <davidd82@usc.edu>
2023-10-19 21:04:34 -0700

    Added dictionary

--- website_parser.py
+++ website_parser.py
@@ -2,27 +2,44 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import urllib.request
 import time
+import pprint
 
-# Playwright is used to simulate mouse clicks and screen scrolling
-# Allows us to trigger JavaScript and capture hidden HTML sections
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto("https://uscinteractionlab.web.app/about/people")
-    time.sleep(2)   # Allow time for page to load
-    page.mouse.wheel(delta_x=0,delta_y=500) # Simulates scrolling to PhD students section
-    time.sleep(2)   # Allow time for page to scroll
-    page.mouse.click(213,500) # Click on coordinates which are relative to scrolling
-    time.sleep(2)   # Allow time for student card to load
-    html = page.content() # Capture page HTML
-    browser.close()
+def get_Phd_info(student_dict, phd_students):
+    # Playwright is used to simulate mouse clicks and screen scrolling
+    # Allows us to trigger JavaScript and capture hidden HTML sections
+    x_coord = 170
+    y_coord = 450 #450
+    for i in range(7):
+        if i == 6:
+            x_coord = 170
+            y_coord = 570
 
-soup = BeautifulSoup(html,'html.parser')
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            page.goto("https://uscinteractionlab.web.app/about/people")
+            time.sleep(1)   # Allow time for page to load
+            page.mouse.wheel(delta_x=0,delta_y=500) # Simulates scrolling to PhD students section
+            time.sleep(0.5)   # Allow time for page to scroll
+            page.mouse.click(x_coord,y_coord) # Click on coordinates which are relative to scrolling
+            time.sleep(0.5)   # Allow time for student card to load
+            html = page.content() # Capture page HTML
+            browser.close()
 
-print(soup.prettify())
+        soup = BeautifulSoup(html,'html.parser')
 
-'''''
-body_tag = soup.body.div.div.p
-print(body_tag.prettify())
-text = body_tag.contents[0]
-print(text)'''
+        #print(soup.prettify())
+
+        body_tag = soup.body.div.p
+        text = body_tag.contents[0]
+        #print(text)
+        student_dict[phd_students[i]] = text
+        x_coord += 200
+
+phd_students = ["Leticia Pinto Alva", "Nathan Dennler", "A'di Dust", 
+                "Mina Kian", "Amy O'Connell", "Zhonghao Shi",
+                "Kaleen Shrestha"]
+
+student_dict = {}
+get_Phd_info(student_dict, phd_students)
+pprint.pprint(student_dict)
