1b577db4985cff3b8a13f34b9a59b2304ccc36a4
David Delgado <davidd82@usc.edu>
2024-02-11 13:18:35 -0800

    Added GOTO audio files

--- src/quori_statemachine.py
+++ src/quori_statemachine.py
@@ -1,86 +1,115 @@
 from typing import Any
 from statemachine import StateMachine, State
-import quori.lab_Students as lab_Students
-
 from playsound import playsound
+import time
+from transitions import Machine
+
+locations = ["../scripts/audio/A'di.mp3",'../scripts/audio/Amy.mp3','../scripts/audio/Kaleen.mp3',
+             '../scripts/audio/Leticia.mp3','../scripts/audio/Mina.mp3','../scripts/audio/Nathan.mp3',
+             '../scripts/audio/Zhonghao.mp3']
+
+people = {
+    "A'di"     : 0,
+    'Amy'      : 1,
+    'Kaleen'   : 2,
+    'Leticia'  : 3,
+    'Mina'     : 4,
+    'Nathan'   : 5,
+    'Zhonghao' : 6,
+}
 
 class LabTourMachine(StateMachine):
     "Quori Lab Tour"
-    greeting = State(initial=True)
-    checking_list = State()
-    moving = State()
-    talking = State()
-    pause = State()
-    outro = State(final=True)
+    
+    # init = State(initial = True)
+    # check_list = State()
+    # moving = State()
+    # check_person = State()
+    # talking = State()
+    # check_person_talking = State()
+    # outro = State(final=True)
 
-    start_tour = greeting.to(checking_list)
+    STATES = ['init', 'check_list', 'moving', 'check_person', 'check_person_talking', 'talking', 'outro']
 
-    check_list = (
-        checking_list.to(outro, cond ="no_more_locations")
-        | checking_list.to(moving, unless="no_more_locations")
-    )
-
-    check_location = (
-        moving.to(talking, cond="reached_location")
-        | moving.to(moving, unless="reached_location")
-    )
-
-    check_person = (
-        talking.to(pause, cond="person_present")
-        | talking.to(checking_list, unless="person_present")
-    )
-
-    check_person_talking = (
-        pause.to(checking_list, cond = "person_finished_talking")
-        | pause.to(pause, unless="person_finished_talking")
-    )
-
-    def __init__(self):
+    
+    def __init__(self, person_there=False, no_more_locations=False):
+        self.machine = Machine(model=self, states=LabTourMachine.STATES, initial='init')
+        self.person_there = person_there
+        self.no_more_locations = no_more_locations
         self.locations = 6
-        super(LabTourMachine, self).__init__()
-        playsound('../scripts/speech.mp3')
+        playsound('../scripts/audio/intro.mp3')
+    
+        self.machine.add_transition(trigger='proceed',source='init', dest='check_list')
+
+        self.machine.add_transition(trigger='proceed',source='check_list', dest='outro', conditions=["no_more_locations_"])
+        self.machine.add_transition(trigger='proceed',source='check_list', dest='moving')
 
 
-    def no_more_locations(self):
-        return self.locations == 0
-    
-    def reached_location(self):
-        return True
-    
-    def person_present(self):
-        return True
-    
-    def person_finished_talking(self):
-        return True
-    
-    def before_start_tour(self):
-        print("Starting Tour!")
+        self.machine.add_transition(trigger='proceed',source='moving', dest='check_person')
 
-    def on_enter_checking_list(self):
+        self.machine.add_transition(trigger='proceed',source='check_person', dest='check_person_talking', conditions=['person_is_there'])
+        self.machine.add_transition(trigger='proceed',source='check_person', dest='talking')
+
+        self.machine.add_transition(trigger='proceed',source='check_person_talking', dest='check_list')
+
+        self.machine.add_transition(trigger='proceed',source='talking', dest='check_list')
+
+
+    def no_more_locations_(self):
+        return self.no_more_locations
+
+      
+    def person_is_there(self):
+        return self.person_there
+
+        
+    def on_enter_init(self):
+        print("Begin")
+
+    def on_enter_check_person(self):
+        response = input('is there a person?')
+        if response == 'y':
+            self.person_there = True
+        else:
+            self.person_there = False
+
+    def on_enter_check_person_talking(self):
+        response = input('is person done talking?')
+        self.locations -= 1
+        print(self.locations)
+
+    def on_enter_check_list(self):
         print("Checking List!")
+        if self.locations == -1:
+            self.no_more_locations = True
+        else:
+            self.no_more_locations = False
     
     def on_enter_outro(self):
         print("No more locations! Saying outro!")
+        playsound('../scripts/audio/outro.mp3')
 
     def on_enter_moving(self):
         print("Moving to next location!")
+        response = input("Give command?")
+        if response == 'y':
+            person = input("Move to who's desk?")
+            self.locations = people[person]
+        else:
+            input('is astro done moving?')
 
     def on_enter_talking(self):
         print("Talking!")
-        print(lab_Students.labStudentDescriptions[lab_Students.phd_students[self.locations]])
+        playsound(locations[self.locations])
         self.locations -= 1
+        print(self.locations)
+
     
-    def on_enter_pause(self):
-        print("Person is talking!")
 
 q = LabTourMachine()
-q.start_tour()
-q.check_list()
-q.check_location()
-q.check_person()
-q.check_person_talking()
-q.check_list()
-q.check_location()
-q.check_person()
-q.check_person_talking()
+
+while q.state != "outro":
+    q.proceed()
+
+
 
